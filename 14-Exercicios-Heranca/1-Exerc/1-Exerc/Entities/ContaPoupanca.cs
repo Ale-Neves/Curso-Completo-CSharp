f09d0d340430e8b4dcebacce87f4c5ba4d99e521
Alexandre Neves <alexandreneves98@outlook.com>
2021-02-10 22:17:59 -0300

    Conceitos de UpCasting e DownCasting

---
+++ 14-Exercicios-Heranca/1-Exerc/1-Exerc/Entities/ContaPoupanca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1_Exerc.Entities
+{
+    class ContaPoupanca : Conta
+    {
+        public double TaxaJuros { get; set; }
+
+        public ContaPoupanca()
+        { }
+        public ContaPoupanca(int numero, string titular, double saldo, double taxaJuros)
+            : base (numero, titular, saldo)
+        {
+            TaxaJuros = taxaJuros;
+        }
+
+        public void AtualizaSaldo(double quantia)
+        {
+            Saldo += quantia * TaxaJuros;
+        }
+    }
+}
