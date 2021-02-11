4ec9095da65338c4061508e06feb49104ebbd460
Alexandre Neves <alexandreneves98@outlook.com>
2021-02-10 21:28:36 -0300

    Conceito de Herança

---
+++ 14-Exercicios-Heranca/1-Exerc/1-Exerc/Entities/ContaDeNegocio.cs
@@ -0,0 +1,23 @@
+﻿namespace _1_Exerc.Entities
+{
+    class ContaDeNegocio : Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaDeNegocio()
+        { }
+        public ContaDeNegocio(int numero, string titular, double saldo, double limitEmp) 
+                       : base(numero, titular, saldo)
+        {
+            LimiteEmprestimo = limitEmp;
+        }
+
+        public void Emprestimo(double quantia)
+        {
+            if(quantia <= LimiteEmprestimo)
+            {
+                Saldo += quantia;
+            }
+        }
+    }
+}
---
+++ 14-Exercicios-Heranca/1-Exerc/1-Exerc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using _1_Exerc.Entities;
+namespace _1_Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Numero Conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Nome Titular: ");
+            string nome = Console.ReadLine();
+            Console.Write("Saldo da Conta: ");
+            double saldo = double.Parse(Console.ReadLine());
+            Console.Write("Limite de Emprestimo: ");
+            double limite = double.Parse(Console.ReadLine());
+
+            ContaDeNegocio ctNegocio = new ContaDeNegocio(numero, nome, saldo, limite);
+
+            Console.WriteLine("\n\n----------------------- ");
+            Console.WriteLine("Numero Conta: " + ctNegocio.Numero);
+            Console.WriteLine("Nome Titular: " + ctNegocio.Titular);
+            Console.WriteLine("Saldo da Conta: " + ctNegocio.Saldo);
+            Console.WriteLine("Limite de Emprestimo: " + ctNegocio.LimiteEmprestimo);
+        }
+    }
+}
