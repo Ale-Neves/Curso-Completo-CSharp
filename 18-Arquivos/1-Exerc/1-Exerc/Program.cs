69aad9151993f0413c0ef930c169eae3ce975b6e
Alexandre Neves <alexandreneves98@outlook.com>
2021-03-02 18:56:51 -0300

    File, FileInfo e IOException

---
+++ 18-Arquivos/1-Exerc/1-Exerc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _1_Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoOrigem = @"c:\Users\alexa\Desktop\Curso de c# Completo\18-Arquivos\arquivo.txt";
+            string caminhoDestino = @"C:\Users\alexa\Desktop\Curso de c# Completo\18-Arquivos\arquivo2.txt";
+
+            try
+            {
+                FileInfo chamaArquivo = new FileInfo(caminhoOrigem);
+                chamaArquivo.CopyTo(caminhoDestino);
+
+                string[] linhas= File.ReadAllLines(caminhoOrigem);
+                foreach(string linha in linhas)
+                {
+                    Console.WriteLine(linha);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
