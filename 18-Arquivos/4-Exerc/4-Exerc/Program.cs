1bdfa911543b3e31b21669bbd19a2e1638983f96
Alexandre Neves <alexandreneves98@outlook.com>
2021-03-03 18:49:14 -0300

    Stream Writer

---
+++ 18-Arquivos/4-Exerc/4-Exerc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _4_Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivoOrigem = @"C:\Users\alexa\Desktop\Curso de c# Completo\18-Arquivos\arquivo.txt";
+            string arquivoDestino = @"C:\Users\alexa\Desktop\Curso de c# Completo\18-Arquivos\arquivo3.txt";
+
+            try
+            {
+                //Lendo as linhas e guardando no array.
+                string[] linhas = File.ReadAllLines(arquivoOrigem);
+
+                using (StreamWriter sw = File.AppendText(arquivoDestino))
+                {
+                    foreach(string linha in linhas)
+                    {
+                        sw.WriteLine(linha.ToUpper());
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
