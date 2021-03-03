1329f646c8b5f21d9b8c4754bf62b7b3a21f55ad
Alexandre Neves <alexandreneves98@outlook.com>
2021-03-02 22:35:46 -0300

    Bloco Using

---
+++ 18-Arquivos/3-Exerc/3-Exerc/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace _3_Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminhoArquivo = @"C:\Users\alexa\Desktop\Curso de c# Completo\18-Arquivos\arquivo.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(caminhoArquivo, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string linha = sr.ReadLine();
+                            Console.WriteLine(linha);
+                        }
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+            //EXEMPLO 2 RESUMIDO
+            try
+            {
+                using (StreamReader sr = File.OpenText(caminhoArquivo))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string linha = sr.ReadLine();
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+        }
+    }
+}
