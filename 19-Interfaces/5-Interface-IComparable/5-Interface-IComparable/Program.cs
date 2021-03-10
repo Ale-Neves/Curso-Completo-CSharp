48de5ee827e4c9645752fa02a6540a2ae3533207
Alexandre Neves <alexandreneves98@outlook.com>
2021-03-09 21:42:22 -0300

    Lendo arquivos e imprimindo Ordenados

---
+++ 19-Interfaces/5-Interface-IComparable/5-Interface-IComparable/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _5_Interface_IComparable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\alexa\Desktop\Curso de c# Completo\19-Interfaces\5-Interface-IComparable\in.txt";
+
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while(!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    // Ordena a lista.
+                    list.Sort();
+                    
+                    foreach(string str in list)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+        }
+    }
+}
