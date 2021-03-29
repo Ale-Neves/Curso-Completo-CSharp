f7e3c19ec0430a329535ebf18890c0aaf2438206
Alexandre Neves <alexandreneves98@outlook.com>
2021-03-29 15:33:59 -0300

    Resolução sem uso de Restrições Generics

---
+++ 20-Generics/2-Exerc/2-Exerc/Services/CalculationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_Exerc.Services
+{
+    class CalculationService
+    {
+        public int Max(List<int> list)
+        {
+            if (list.Count == 0)
+                throw new ArgumentException("The list can not be empty");
+
+            int max = list[0];
+            for(int i=0; i<list.Count; i++)
+            {
+                if(list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+
+            return max;
+        }
+
+    }
+}
