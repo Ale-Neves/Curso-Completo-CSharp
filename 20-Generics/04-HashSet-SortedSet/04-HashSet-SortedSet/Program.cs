316ae23c3a8563f4f73cdb5950d57beb469e79ca
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-02 16:48:06 -0300

    Introdução ao HashSet

---
+++ 20-Generics/04-HashSet-SortedSet/04-HashSet-SortedSet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_HashSet_SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando objeto
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("Tv");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            //Percorrendo o conjunto de elementos e verificando se contem Computador
+            Console.WriteLine(set.Contains("Computador")); // retornara: false;
+
+            //percorrendo os elementos
+            foreach(string p in set)
+            {
+                // imprimindo todos elementos.
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
