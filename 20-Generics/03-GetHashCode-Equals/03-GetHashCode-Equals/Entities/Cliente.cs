913277dca13c453c331c61f0126a218a3ec35289
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-02 16:15:52 -0300

    Introdução ao GetHashCode e Equals

---
+++ 20-Generics/03-GetHashCode-Equals/03-GetHashCode-Equals/Entities/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_GetHashCode_Equals.Entities
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Cliente()
+        { }
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Cliente))
+                return false;
+
+            // other esta recebendo o objto como cliente(Conversão DownCasting)
+            Cliente other = obj as Cliente;
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ 20-Generics/03-GetHashCode-Equals/03-GetHashCode-Equals/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using _03_GetHashCode_Equals.Entities;
+
+namespace _03_GetHashCode_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente a = new Cliente { Nome = "Alexandre", Email = "alexandre@gmail.com" };
+            Cliente b = new Cliente { Nome = "Gian", Email = "gian@gmail.com" };
+
+            Console.WriteLine(a.Equals(b)); // return = false
+            Console.WriteLine(a.GetHashCode()); // return = -123256643 (valor aleatorio)
+
+            // Essa comparação é diferente pois comprara com a posição da memoria a outra compara valor.
+            Console.WriteLine(a == b); 
+        }
+    }
+}
