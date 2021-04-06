083d43cd018822a6d55d060dd0797808f5fdf384
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-05 22:52:47 -0300

    Exercicio 02 utilização de coleções

---
+++ 20-Generics/07-Exerc-Colecoes/07-Exerc-Colecoes/Alunos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07_Exerc_Colecoes
+{
+    class Alunos
+    {
+        public int CodigoAluno { get; set; }
+
+        public Alunos()
+        {
+
+        }
+        public Alunos(int codigo)
+        {
+            CodigoAluno = codigo;
+        }
+
+        public override int GetHashCode()
+        {
+            return CodigoAluno.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Alunos))
+                return false;
+
+            Alunos other = obj as Alunos;
+            return CodigoAluno.Equals(other.CodigoAluno);
+        }
+    }
+}
---
+++ 20-Generics/07-Exerc-Colecoes/07-Exerc-Colecoes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07_Exerc_Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Alunos> course = new HashSet<Alunos>();
+
+            Console.Write("How many students for course A?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int codigo = int.Parse(Console.ReadLine());
+                course.Add(new Alunos { CodigoAluno = codigo });
+            }
+
+            Console.Write("How many students for course B?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int codigo = int.Parse(Console.ReadLine());
+                course.Add(new Alunos { CodigoAluno = codigo });
+            }
+
+            Console.Write("How many students for course C?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int codigo = int.Parse(Console.ReadLine());
+                course.Add(new Alunos { CodigoAluno = codigo });
+            }
+
+            Console.WriteLine("Total students: " + course.Count);
+
+
+            /* Resolução do Professor
+             
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) {
+                int cod = int.Parse(Console.ReadLine());
+                courseA.Add(cod);
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) {
+                int cod = int.Parse(Console.ReadLine());
+                courseB.Add(cod);
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++) {
+                int cod = int.Parse(Console.ReadLine());
+                courseC.Add(cod);
+            }
+
+            HashSet<int> all = new HashSet<int>(courseA);
+            all.UnionWith(courseB);
+            all.UnionWith(courseC);
+            Console.WriteLine("Total students: " + all.Count);
+             
+             */
+        }
+    }
+}
