2d9b6f8153a1863641d0178ec259a7c3df18c23c
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-07 20:57:00 -0300

    Introdução Extension Methods

---
+++ 21-ExtensionMethods/1-Demonstração/1-Demonstração/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace _1_Demonstração.Extensions
+{
+    static class DateTimeExtensions
+    {
+        //METODO
+        public static string ElapsedTime(this DateTime thisObjs)
+        {
+            // Pegando a data atual e subtraindo do objto passado por parametro.
+            TimeSpan duration = DateTime.Now.Subtract(thisObjs);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("f1", CultureInfo.InvariantCulture) + "Hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("f2", CultureInfo.InvariantCulture) + "Days";
+            }
+        }
+    }
+}
---
+++ 21-ExtensionMethods/1-Demonstração/1-Demonstração/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using _1_Demonstração.Extensions;
+
+namespace _1_Demonstração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 04, 01, 0, 0, 0);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
