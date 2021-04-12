943a04aad3ef8d785c8f79a76a001877a5b30b90
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-12 19:42:21 -0300

    Introdução Comparison<T>

---
+++ 22-Comparison-T/1-Uma_Experiência_com_Comparison/1-Uma_Experiência_com_Comparison/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1_Uma_Experiência_com_Comparison.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
