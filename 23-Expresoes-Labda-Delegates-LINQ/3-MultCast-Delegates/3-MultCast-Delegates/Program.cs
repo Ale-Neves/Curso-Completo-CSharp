798e95df9ed262652ce6c41377eb5e07bbec760d
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-12 22:29:54 -0300

    Introdução Multicast Delegates

---
+++ 23-Expresoes-Labda-Delegates-LINQ/3-MultCast-Delegates/3-MultCast-Delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using _3_MultCast_Delegates.Services;
+namespace _3_MultCast_Delegates
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            op(a, b);
+        }
+    }
+}
