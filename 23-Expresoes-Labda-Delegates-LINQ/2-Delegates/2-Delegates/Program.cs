3ff7f9600ad8859938fdf943b5b25b4d99a54d99
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-12 21:10:45 -0300

    Instrodução ao Delegates

---
+++ 23-Expresoes-Labda-Delegates-LINQ/2-Delegates/2-Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _2_Delegates.Services;
+
+namespace _2_Delegates
+{
+    //Declarando delegate
+    delegate double BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            
+            // BinaryNumericOperation op = CalculationService.Sum;
+            BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+            // double result = op(a, b);
+            double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
