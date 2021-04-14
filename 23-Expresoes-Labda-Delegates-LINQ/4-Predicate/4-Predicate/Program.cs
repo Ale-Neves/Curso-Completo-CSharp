2661b5c00135d83fe50258903f8bcc04a3c18480
Alexandre Neves <alexandreneves98@outlook.com>
2021-04-13 21:29:13 -0300

    Exemplo sem a utilização do pradicate

---
+++ 23-Expresoes-Labda-Delegates-LINQ/4-Predicate/4-Predicate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using _4_Predicate.Entities;
+
+namespace _4_Predicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando obj do tipo product
+            List<Product> list = new List<Product>();
+
+            //Adicionando a Lista
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            //Remove apartir da expressão lambda
+            list.RemoveAll(p => p.Price >= 100.00);
+
+            //Imprimindo
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
