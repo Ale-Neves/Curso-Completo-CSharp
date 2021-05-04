2021660e20d570cc72b46c823f76ea296489f703
Alexandre Neves <alexandreneves98@outlook.com>
2021-05-03 21:46:19 -0300

    Introdução ao LINQ

---
+++ 23-Expresoes-Labda-Delegates-LINQ/7-Linq/1-Introducao/1-Introducao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1_Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Especificando o data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Definindo a expressão query
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Executa a query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
